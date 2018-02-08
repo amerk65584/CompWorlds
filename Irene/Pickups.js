87ab918d3d69e858de39336303b7bcb1f09396b0
Irene Fransiga <irene.fransiga@yahoo.com>
2018-02-08 13:47:35 -0800

    added bounding box for all the interactions

--- Irene/Pickups.js
+++ Irene/Pickups.js
@@ -32,7 +32,8 @@ Pickup.prototype.update = function () {
 }
 
 Pickup.prototype.draw = function (ctx) {
-   
     this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+    this.ctx.strokeRect(this.x, this.y,this.frameWidth,this.frameHeight);
+
     Entity.prototype.draw.call(this);
 }
