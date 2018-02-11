603049b82813be83a9af452383d513058b5670a3
digitalNightingale <lmr81@uw.edu>
2018-02-11 11:49:00 -0800

    leah is up to date

--- Leah/Background.js
+++ Leah/Background.js
@@ -2,7 +2,7 @@
  * Background Object *
  *********************/
 
-function Background(game, ctx, spritesheet, speed) {
+function Background(game, ctx, spritesheet, speed, pause) {
     this.speed = speed; 
     this.backgroundWidth = 1018;
     this.x = 0;
@@ -10,7 +10,8 @@ function Background(game, ctx, spritesheet, speed) {
     this.spritesheet = spritesheet;
     this.game = game;
     this.ctx = ctx;
-
+    this.entities_copy = [];
+    this.pause = pause;
 	this.draw = function() {
         this.x += this.speed;
          // Scrolling left
@@ -30,4 +31,18 @@ Background.prototype.draw = function () {
 };
 
  Background.prototype.update = function () {
+    if ((this.game.click.x >= 380 && this.game.click.x <= 610) && (this.game.click.y >= 290 && this.game.click.y <= 330)) {
+        for(var i = 1; i < this.pause.entities_copy.length; i++) {
+            this.game.entities[i] = this.pause.entities_copy[i];
+            this.game.entities[i].removeFromWorld = false;
+        }
+        for (var i = 0; i < this.pause.entities_copy.length; i++) {
+            this.pause.entities_copy.pop();
+        }
+        console.log(this.pause.entities_copy);
+        console.log(this.game.entities)
+        this.flag = !this.flag;
+        this.game.click.x = null;
+        this.game.click.y = null;
+    }
 };
