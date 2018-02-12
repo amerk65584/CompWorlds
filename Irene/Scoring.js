59b3ee5b7feb50eabaaaf6f9c718a04970e0085f
Irene Fransiga <irene.fransiga@yahoo.com>
2018-02-12 14:10:56 -0800

    localstorage for highscore is up on irene

--- Irene/Scoring.js
+++ Irene/Scoring.js
@@ -1,6 +1,7 @@
 /*********************
 * Scoring Object *
 *********************/
+window.localStorage;
 function Scoring(game, ctx) {
     this.x = 0;
     this.y = 0;
@@ -8,6 +9,21 @@ function Scoring(game, ctx) {
     this.fps = 60;
     this.game = game;
     this.ctx = ctx;
+}
+function saveScore(score){
+    //console.log(score + " " + Number(localStorage.highscore));
+    if(typeof(Storage) != '"undefined'){
+       if(localStorage.highscore && (score > Number(localStorage.highscore))){
+           localStorage.highscore = score;
+           //localStorage.highscore = 0; //to reset the highscore
+           console.log("HIGHSCORE: " + localStorage.highscore);
+       } else if (localStorage.highscore && (score < Number(localStorage.highscore))){
+            console.log("Not highscore yet");
+            //localStorage.highscore = 0; //to rest the highscore
+       }
+    } else {
+        console.log("Local browser does not support web storage");
+    }
 }
 Scoring.prototype.draw = function(){
     if (this.game.running) {
@@ -18,5 +34,8 @@ Scoring.prototype.draw = function(){
     }
 };
 Scoring.prototype.update = function(){
-    if (this.game.running) this.score += (1/this.fps);
+    if (this.game.running){
+        this.score += (1/this.fps);
+        saveScore(Math.floor(this.score));
+    }
 };
